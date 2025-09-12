47e815f6a8003f746d570f1b04cd1b5898cfe6b5
Marcos Alvarez <marcosalvare@gmail.com>
2025-09-12 17:12:34 +0200

    Creates URLSession data task with URL on 'URLSessionHTTPClient.get(from: URL)'

---
+++ EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -0,0 +1,52 @@
+//
+//  URLSessionHTTPClientTests.swift
+//  EssentialFeed
+//
+//  Created by Israel Marcos Alvarez Mesa on 12/9/25.
+//
+
+import XCTest
+
+class URLSessionHTTPClient {
+    private let session: URLSession
+    
+    init(session: URLSession = .shared) {
+        self.session = session
+    }
+    
+    func get(from url: URL) {
+        session.dataTask(with: url) { _, _, _ in }
+    }
+}
+
+
+class URLSessionHTTPClientTests: XCTestCase {
+    
+    func test_getFromURL_createsDataTaskWithURL() {
+        
+        let url = URL(string: "http://any-url.com")!
+        let session = URLSessionSpy()
+        
+        let sut = URLSessionHTTPClient(session: session)
+        sut.get(from: url)
+        
+        XCTAssertEqual(session.receivedURLs, [url])
+    }
+    
+    // MARK: Helpers
+    
+    private class URLSessionSpy: URLSession, @unchecked Sendable {
+        
+        var receivedURLs = [URL]()
+        
+        override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+            receivedURLs.append(url)
+            return FakeURLSessionDataTask()
+        }
+    }
+    
+    private class FakeURLSessionDataTask: URLSessionDataTask, @unchecked Sendable {
+        
+        override func resume() {}
+    }
+}
