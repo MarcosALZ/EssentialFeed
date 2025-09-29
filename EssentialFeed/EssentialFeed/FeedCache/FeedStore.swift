2ec6881c5bdb658047f9032e8595142c29d11de0
Marcos Alvarez <marcosalvare@gmail.com>
2025-09-29 20:52:14 +0200

    Moved 'FeedStore' to its own file

---
+++ EssentialFeed/EssentialFeed/FeedCache/FeedStore.swift
@@ -0,0 +1,16 @@
+//
+//  FeedStore.swift
+//  EssentialFeed
+//
+//  Created by Israel Marcos Alvarez Mesa on 29/9/25.
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+    
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
