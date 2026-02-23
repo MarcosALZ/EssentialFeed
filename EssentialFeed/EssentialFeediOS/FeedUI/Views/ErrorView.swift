4b579cd73051eb8a6fa510bb9dd3bcc1437020dc
Marcos Alvarez <marcosalvare@gmail.com>
2026-02-23 18:18:34 +0100

    Move `ErrorView` to separate file

---
+++ EssentialFeed/EssentialFeediOS/FeedUI/Views/ErrorView.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorView.swift
+//  EssentialFeed
+//
+//  Created by Israel Marcos Alvarez Mesa on 23/2/26.
+//
+
+import UIKit
+
+public final class ErrorView: UIView {
+    public var message: String?
+}
