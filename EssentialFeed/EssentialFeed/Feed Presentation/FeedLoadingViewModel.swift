737c388b8462439e80899ada2f9d63cde1301bf5
Marcos Alvarez <marcosalvare@gmail.com>
2026-02-28 21:25:14 +0100

    Move `FeedLoadingViewModel` to separate file

---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeed
+//
+//  Created by Israel Marcos Alvarez Mesa on 28/2/26.
+//
+
+public struct FeedLoadingViewModel {
+    public let isLoading: Bool
+}
